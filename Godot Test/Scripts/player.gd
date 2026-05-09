8b87e6b03824cb8b7b0c408e0185ad18c11eb635
Michael-dev0 <mdshu6@gmail.com>
2026-05-09 18:41:58 -0400

    updateing to git mode

--- Godot Test/Scripts/player.gd
+++ Godot Test/Scripts/player.gd
@@ -7,7 +7,8 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# making changes to test git
+# making changes to test git again 
+# change here
 
 func _physics_process(delta):
 	# Add the gravity.
